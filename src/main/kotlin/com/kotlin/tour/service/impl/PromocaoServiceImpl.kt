2c8f8bfbf2ca1b8517300eb0b4a9b14b9f52dd42
Luiz Geraldo Pinheiro <luiz_geraldo@sicredi.com.br>
2020-09-28 18:46:01 -0300

    Add Cache

--- src/main/kotlin/com/kotlin/tour/service/impl/PromocaoServiceImpl.kt
+++ src/main/kotlin/com/kotlin/tour/service/impl/PromocaoServiceImpl.kt
@@ -3,6 +3,8 @@ package com.kotlin.tour.service.impl
 import com.kotlin.tour.model.Promocao
 import com.kotlin.tour.repository.PromocaoRepository
 import com.kotlin.tour.service.PromocaoService
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Component
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Component
 @Component
 class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository) : PromocaoService {
 
+    @CacheEvict("promocoes", allEntries = true)
     override fun create(promocao: Promocao) {
         this.promocaoRepository.save(promocao)
     }
@@ -18,10 +21,12 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository) : Promocao
         return this.promocaoRepository.findById(id).orElseGet(null)
     }
 
+    @CacheEvict("promocoes", allEntries = true)
     override fun delete(id: Long) {
         this.promocaoRepository.deleteById(id)
     }
 
+    @CacheEvict("promocoes", allEntries = true)
     override fun update(id: Long, promocao: Promocao) {
         create(promocao)
     }
@@ -29,6 +34,7 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository) : Promocao
     override fun searchByLocal(local: String): List<Promocao> =
             listOf()
 
+    @Cacheable("promocoes")
     override fun getAll(start: Int, size: Int): List<Promocao> {
         val pages = PageRequest.of(start, size, Sort.by("local").ascending())
         return this.promocaoRepository.findAll(pages).toList()
